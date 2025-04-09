00604736d27e3883382d24bb9c7f95f24f6effca
Jean Pierretype <jeanpi_2504@hotmail.com>
2025-04-09 10:21:41 -0500

    Tercer commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.Write("Bienvenidos al curso de c#");
             Console.Write("Segundo intento");
+            Console.Write("Tercer intento");
             Console.ReadKey();
 
 
